d5aa64d7100d4dde4bacd4b1faf6c4abe9aaab6a
AshMa1022 <wma@risd.edu>
2023-11-11 22:19:17 -0500

    update chap4

--- js/menu.js
+++ js/menu.js
@@ -4,7 +4,8 @@
     var isHidden = window.getComputedStyle(sec).display === 'none';
     if (isHidden) {
       console.log(1);
-      sec.style.display = 'block'; // or 'flex' or 'grid', depending on your layout
+
+      sec.style.display = 'flex'; // or 'flex' or 'grid', depending on your layout
       // Trigger reflow before adding the class for the transition
       sec.offsetHeight; // This line is necessary to force a reflow
       sec.classList.add('subChap_visible');
@@ -22,13 +23,8 @@
     var menu = document.querySelector(".menu");
     menu.style.opacity = 0;
     var galleryc = document.querySelector(".img_gallery_container");
-    var contentDiv = document.getElementById('sec' + sectionNumber);
-    // if (contentDiv) {
-    //   contentDiv.style.display = 'none';
-    //   if(gallery){
-    //     gallery.style.display ='none';
-    //   }
-    // }
+    var contentDiv = document.querySelector('.sec');
+
     galleryc.innerHTML='';
     contentDiv.style.display='none';
     if(sectionNumber == 5){
@@ -36,7 +32,7 @@
     }
     var menu = document.querySelector('.menu');
     var intro = document.querySelector('.intro');
-    var newContentDiv = document.querySelector('.allChap');
+    var newContentDiv = document.querySelector('.side_nav');
     if (newContentDiv) {
       newContentDiv.style.display = 'flex';
       intro.style.display = 'block';

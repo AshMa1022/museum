52e37c4d323396a88776e1b473afe346463b7b42
Luke <lllvyp@gmail.com>
2024-01-28 17:31:26 +0800

    add displaycase.css

---
+++ js/displaycase.js
@@ -0,0 +1,15 @@
+const exhibits = document.querySelectorAll('.display-case-exhibit');
+
+// Add a click event listener to each "image-container" element
+exhibits.forEach((exhibit) => {
+  exhibit.addEventListener('click', () => {
+    // Toggle the "bigger" class on the clicked element
+    if (exhibit.classList.contains('bigger')) {
+      exhibit.classList.remove('bigger');
+    } else {
+      enlargedExhibits = document.querySelectorAll('.display-case-exhibit.bigger');
+      enlargedExhibits.forEach((e) => e.classList.remove('bigger'));
+      exhibit.classList.add('bigger');
+    }
+  });
+});

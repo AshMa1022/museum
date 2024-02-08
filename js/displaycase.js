0fe1f41938fdaa62f42b1542a67c9daa8ac04047
Luke <lllvyp@gmail.com>
2024-02-08 20:20:39 +0800

    displaycase add transition

--- js/displaycase.js
+++ js/displaycase.js
@@ -10,6 +10,11 @@ exhibits.forEach((exhibit) => {
       enlargedExhibits = document.querySelectorAll('.display-case-exhibit.bigger');
       enlargedExhibits.forEach((e) => e.classList.remove('bigger'));
       exhibit.classList.add('bigger');
+
+      const yOffset = -140;
+      const y = exhibit.getBoundingClientRect().top + window.scrollY + yOffset;
+
+      window.scrollTo({ top: y });
     }
   });
 });
